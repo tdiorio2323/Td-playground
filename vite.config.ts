98addb5f02198f1efa28d10b8cf636569715c64b
tyler <tyler@tdstudiosny.com>
2025-09-11 11:10:04 +0000

    ss w

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    hmr: {
+      protocol: 'wss',
+      clientPort: 443
+    }
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
